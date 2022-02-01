a8279bdb11a2e535a7f4cb32457979d41a0ec52c
Satish Nayak <satish_nayak@epam.com>
2022-02-01 11:21:31 +0530

    Initial Setup

---
+++ src/main/java/javafunctional/TestDrive.java
@@ -0,0 +1,11 @@
+package javafunctional;
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
